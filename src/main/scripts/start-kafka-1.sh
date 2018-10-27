42f7f4f030223ae9fa592bb92670b1836d3fb978
Neil Stevenson <neilstevenson@users.noreply.github.com>
2018-10-27 11:36:33 +0100

    Saturday morning

---
+++ src/main/scripts/start-kafka-1.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd /Applications/kafka_2.11-2.0.0/bin
+
+# Run with "localhost" so Kafka is available when laptop has no network
+./kafka-server-start.sh ../config/server.properties \
+	--override advertised.host.name=localhost \
+	--override broker.id=1 \
+	--override log.dirs=/tmp/kafka-logs-1 \
+	--override port=9093
